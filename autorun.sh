3126d180f6647193fe39f2a3f65612d531563eb6
Trần Thê Vinh <mrtranthevinh@gmail.com>
2025-10-26 19:10:36 +0700

    Update GitHub repository URL for GRUB fix

--- autorun.sh
+++ autorun.sh
@@ -2,6 +2,6 @@
 set -e
 echo "[*] Cloning and running auto GRUB fix..."
 TMPDIR=$(mktemp -d)
-git clone https://github.com/mrthevinh/ubuntu-auto-fixgrub.git "$TMPDIR"
+git clone https://github.com/mrthevinh/fixgrub.git "$TMPDIR"
 cd "$TMPDIR"
 sudo bash fix-grub-ubuntu.sh
